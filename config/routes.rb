8c2309efd63088014ff888e5458590ebf589008f
EC2 Default User <ec2-user@ip-172-31-20-68.ec2.internal>
2021-08-30 04:29:11 +0000

    応用課題6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "homes#top"
   get "home/about" => "homes#about"
+  get "/search" => "searches#search"
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
