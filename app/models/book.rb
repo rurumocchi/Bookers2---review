d58d2deb44f1c9fc29aa026bd41a48992e4a9dd5
EC2 Default User <ec2-user@ip-172-31-20-68.ec2.internal>
2021-09-03 09:30:51 +0000

    応用課題7d

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,11 @@ class Book < ApplicationRecord
 	has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+  validates :rate, presence: true, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }
+
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
